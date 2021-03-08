ffc2bca6ff62bec62c265691a7506e28b99eed88
Marlom <falconsoft.3d@gmail.com>
2021-03-08 18:13:21 +0100

    sc

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
 const App = ()  => {
+
+  const [citas, setCitas] = useState([
+    { id: "1", paciente: "Hook", propietario: 'Juan', sintomas: "No come" },
+    { id: "2", paciente: "Rosa", propietario: 'Juan', sintomas: "No come" },
+    { id: "3", paciente: "Maria", propietario: 'Juan', sintomas: "No come" }
+  ]);
+
   return (
-    <Text   style= {styles.titulo}  >  Administrador de Cita</Text>
+    <View style= {styles.contenedor} >
+      <Text style= {styles.titulo} >Administrador de Citas</Text>
+          { citas.map(cita => (
+            <View>
+              <Text>{cita.paciente}</Text>
+              <Text>ss</Text>
+            </View>
+          ))}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  titulo: {
+  contenedor: {
+    backgroundColor: '#AA076B',
+    flex: 1
+  },
 
+  titulo: {
+    color: '#FFF',
+    marginTop: 40,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
   }
-})
+});
 
 export default App;
